9f0b566247b72f92a5956d06edd1740ec74ecf3c
Luís PC_CASA <luishfmendes226@gmail.com>
2026-09-26 00:08:01 -0300

    feat(projeto): adiciona estrutura inicial do backend utilizando Spring

---
+++ backend/src/main/java/com/inovatech/portal_instituto_caapora/PortalInstitutoCaaporaApplication.java
@@ -0,0 +1,13 @@
+package com.inovatech.portal_instituto_caapora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalInstitutoCaaporaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalInstitutoCaaporaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/inovatech/portal_instituto_caapora/PortalInstitutoCaaporaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inovatech.portal_instituto_caapora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalInstitutoCaaporaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
